b38cb07a4c7fb47a594b3945d123130e637f2f17
Luca <memeoluca6@gmail.com>
2021-03-23 16:33:03 +0100

    added color to icons

--- js/script.js
+++ js/script.js
@@ -118,20 +118,30 @@ const icons = [
   },
 ];
 
-iconHtml = $(".container .icons")
-
-icons.forEach((element) => {
-  let html = `<div>
-    <i class="${element.family} ${element.prefix}${element.name}"></i>
-    <div class="title">${element.name}</div>
-  </div>`
-  iconHtml.append(html);
-});
-
 const categories = [];
 
 icons.forEach((element) => {
   if(categories.includes(element.category) == false) {
     categories.push(element.category);
   }
+});
+
+const colors = ["red", "purple", "black"]
+
+iconHtml = $(".container .icons")
+
+icons.forEach((element) => {
+  if(element.category == categories[0]) {
+    element["color"] = colors[0];
+  } else if(element.category == categories[1]) {
+    element["color"] = colors[1];
+  } else {
+    element["color"] = colors[2];
+  }
+  console.log(element);
+  let html = `<div style="color:${element.color}">
+    <i class="${element.family} ${element.prefix}${element.name}"></i>
+    <div class="title">${element.name}</div>
+  </div>`
+  iconHtml.append(html);
 });
